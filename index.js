6dd7ea921f006fffc6887cedc90d41ac085faedf
Salima Mukhit <smukhit@dons.usfca.edu>
2025-02-20 13:27:47 -0800

    initialized a git repo

---
+++ index.js
@@ -0,0 +1,185 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const cors = require('cors');
+const { Server } = require("socket.io");
+const { randomBytes } = require('crypto');
+const { initialBoard } = require('./initialBoard.js');
+const mongoose = require('mongoose');
+const mongodb = mongoose.connect(process.env.MONGOOSE_URL);
+const cookieParser = require('cookie-parser');
+
+const gameSchema = new mongoose.Schema({
+  gameId: String,
+  whitePlayer: Object, // token and username and updating
+  blackPlayer: Object, // token and username and updating
+  gameBoard: Object,
+  playerTurn: String,
+  private: Boolean,
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(cookieParser());
+
+mongodb.then(mongo => {
+  app.get('/', async (req, res) => {
+    res.send("Welcome to checkers backend");
+  });
+  
+  app.get('/activeGames', async (req, res) => {
+    try {
+      const games = await Game.find();
+      const activeGameIds = [];
+      games.forEach(game => {
+        if ((!game.whitePlayer || !game.blackPlayer) && !game.private) {
+          activeGameIds.push(game.gameId);
+        }
+      });
+      res.status(200).send({ activeGameIds });
+    } catch (e) {
+      res.status(500).send({ message: 'Unknown server error occurred' });
+      console.error(e);
+    }
+  });
+  
+  // create a new game, get the id back
+  app.post('/game', async (req, res) => {
+    const { private } = req.body;
+    try {
+      const game = await Game.create({
+        gameId: randomBytes(32).toString('base64').replace(/[^a-zA-Z0-9]/g, '').slice(0, 10),
+        gameBoard: initialBoard,
+        playerTurn: 'whitePlayer',
+        private: Boolean(private),
+      });
+      // if the game is public we want to notify connected clients that it was created
+      if (!private) {
+        io.emit('game created', { gameId: game.gameId });
+      }
+      res.status(200).send({ gameId: game.gameId });
+    } catch (e) {
+      res.status(500).send({ message: 'Unknown server error occurred' });
+      console.error('Unknown server error occurred', e);
+    }
+  });
+  
+  app.get('/game', async (req, res) => {
+    try {
+      const { gameId } = req.query;
+      const games = await Game.find({ gameId });
+      if (games.length === 0) {
+        res.status(404).send({ message: 'Game with such id was not found' });
+      }
+      res.send({ gameBoard: games[0].gameBoard });
+    } catch (e) {
+      res.status(500).send({ message: 'Unknown server error occurred' });
+      console.error('Unknown server error occurred', e);
+    }
+  });
+
+  app.post('/game/connectPlayer', async (req, res) => {
+    try {
+      const { gameId, username } = req.body;
+      if (!gameId) {
+        res.status(400).send({ message: 'Must provide valid gameId!' });
+      }
+
+      const game = (await Game.find({ gameId }))[0];
+      if (!game) {
+        res.status(404).send({ message: 'Game with such id was not found' });
+      }
+
+      const gameIdCookie = req.cookies[`typicalCheckersGameId${gameId}`];
+      if (gameIdCookie) {
+        if (game.whitePlayer?.token === gameIdCookie) {
+          res.status(200).send({ playerTurn: 'whitePlayer' });
+        } else if (game.blackPlayer?.token === gameIdCookie) {
+          res.status(200).send({ playerTurn: 'blackPlayer' });
+        } else {
+          res.status(401).send({ message: 'You are not authorized to join this game' });
+        }
+      }
+
+      // if the existing player is not reconnecting and the game is full then it is not possible to connect
+      if (game.whitePlayer && game.blackPlayer) {
+        res.status(401).send({ message: 'This game is at full capacity' });
+      }
+
+      // if the game is not at full capacity, provide the player for the user
+      if (!username) {
+        res.status(401).send({ message: 'Must provide a valid username to join a game' });
+      }
+      if (!game.whitePlayer) {
+        game.set('whitePlayer', { updating: true });
+        await game.save();
+        game.set('whitePlayer', {
+          token: randomBytes(32).toString('base64').replace(/[^a-zA-Z0-9]/g, '').slice(0, 7),
+          username,
+          updating: false,
+        });
+        await game.save();
+        res.cookie(`typicalCheckersGameId${gameId}`, game.whitePlayer.token, {
+          maxAge: 86400000, // Cookie expires in 24 hours
+          httpOnly: true, // Cookie is not accessible via JavaScript
+          secure: false, // Set to true if using HTTPS
+        });
+        res.status(200).send({ playerTurn: 'whitePlayer' });
+      } else if (!game.blackPlayer) {
+        game.set('blackPlayer', { updating: true });
+        await game.save();
+        game.set('blackPlayer', {
+          token: randomBytes(32).toString('base64').replace(/[^a-zA-Z0-9]/g, '').slice(0, 7),
+          username,
+          updating: false,
+        });
+        await game.save();
+        res.cookie(`typicalCheckersGameId${gameId}`, game.blackPlayer.token, {
+          maxAge: 86400000, // Cookie expires in 24 hours
+          httpOnly: true, // Cookie is not accessible via JavaScript
+          secure: false, // Set to true if using HTTPS
+        });
+        res.status(200).send({ playerTurn: 'blackPlayer' });
+      }
+    } catch (e) {
+      res.status(500).send({ message: 'Unknown server error occurred' });
+      console.error('Unknown server error occurred', e);
+    }
+  });
+  
+  // validate player from cookies
+  // validate the player turn
+  app.post('/game/tileClick', async (req, res) => {
+    const { gameId } = req.body;
+    const game = (await Game.find({ gameId }))[0];
+    if (!game) {
+      res.status(401).send({ message: 'Game with such id was not found' });
+    }
+    const playerToken = req.cookies[`typicalCheckersGameId${gameId}`];
+    if (!playerToken) {
+      res.status(401).send({ message: 'You are not authorized to join this game' });
+    }
+    if (playerToken === game.whitePlayer.token && game.playerTurn === 'whitePlayer') {
+      // logic
+      // io.emit(`board updated ${gameId}`)
+    } else if (playerToken === game.whitePlayer.token && game.playerTurn === 'whitePlayer') {
+      //logic
+      // io.emit(`board updated ${gameId}`)
+    }
+  });
+});
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
---
+++ initialBoard.js
@@ -0,0 +1,50 @@
+export const initialBoard = {
+  1: {
+    1: 'blackPlayer',
+    3: 'blackPlayer',
+    5: 'blackPlayer',
+    7: 'blackPlayer',
+  },
+  2: {
+    2: 'blackPlayer',
+    4: 'blackPlayer',
+    6: 'blackPlayer',
+    8: 'blackPlayer',
+  },
+  3: {
+    1: 'blackPlayer',
+    3: 'blackPlayer',
+    5: 'blackPlayer',
+    7: 'blackPlayer',
+  },
+  4: {
+    2: null,
+    4: null,
+    6: null,
+    8: null,
+  },
+  5: {
+    1: null,
+    3: null,
+    5: null,
+    7: null,
+  },
+  6: {
+    2: 'whitePlayer',
+    4: 'whitePlayer',
+    6: 'whitePlayer',
+    8: 'whitePlayer',
+  },
+  7: {
+    1: 'whitePlayer',
+    3: 'whitePlayer',
+    5: 'whitePlayer',
+    7: 'whitePlayer',
+  },
+  8: {
+    2: 'whitePlayer',
+    4: 'whitePlayer',
+    6: 'whitePlayer',
+    8: 'whitePlayer',
+  },
+};
